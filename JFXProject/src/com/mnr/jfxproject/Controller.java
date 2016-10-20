a3d54c1608be44516140d582bc20804b6fece1ee
manor9421 <manor9421@yahoo.co.jp>
2016-10-20 10:43:22 +0300

    add css

--- JFXProject/src/com/mnr/jfxproject/Controller.java
+++ JFXProject/src/com/mnr/jfxproject/Controller.java
@@ -1,5 +1,14 @@
 package com.mnr.jfxproject;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
+public class Controller {
+	
+	@FXML private TextField firstName;
+
+	public void handleSaySup(ActionEvent event){
+		System.out.printf("Pressed %s! %n", firstName.getText());
+	}
 }
