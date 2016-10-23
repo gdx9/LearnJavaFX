b6811bccfc9b6bbe890eb50fa3510c5f6617ccd0
manor9421 <manor9421@yahoo.co.jp>
2016-10-23 15:09:56 +0300

    fix exit on close bug

--- DBJavaFXProject/src/main/java/com/mnr/dbjavafxproject/Main.java
+++ DBJavaFXProject/src/main/java/com/mnr/dbjavafxproject/Main.java
@@ -3,7 +3,10 @@ package com.mnr.dbjavafxproject;
 import com.mnr.dbjavafxproject.view.WindowManipulator;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class Main extends Application {
@@ -13,6 +16,16 @@ public class Main extends Application {
 		WindowManipulator wm = new WindowManipulator(primaryStage);
 		wm.drawMainScene();
 		
+		//exit on close
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+			@Override
+			public void handle(WindowEvent event) {
+				Platform.exit();
+				System.exit(0);
+			}
+			
+		});
 	}
 	
 	public static void main(String[] args) {
